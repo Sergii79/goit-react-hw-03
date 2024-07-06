874c0bcd379215cfdb8c48de292bcf4010bc35e9
Sergii79 <tsymbal.sergiy@gmail.com>
2024-07-06 10:10:08 +0200

    commit second

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,21 @@
 import css from "../Contact/Contact.module.css";
+import { FaUser, FaPhoneAlt } from "react-icons/fa";
 
 export default function Contact({ contact }) {
   return (
-    <li className={css.list}>
-      <p>
-        {contact.name}: {contact.number}
-      </p>
-      <button>Delete</button>
+    <li className={css.container}>
+      <div className={css.list}>
+        <p className={css.contactItem}>
+          <FaUser />
+          {contact.name}
+        </p>
+        <p className={css.contactItem}>
+          <FaPhoneAlt />
+          {contact.number}
+        </p>
+      </div>
+
+      <button className={css.button}>Delete</button>
     </li>
   );
 }
